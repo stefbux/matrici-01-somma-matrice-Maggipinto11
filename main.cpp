f43baf904529c483e080e4e1fcae725a61d002ca
Maggipinto11 <pietroandrea.maggipinto@marconibari.edu.it>
2025-03-13 09:13:24 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main() 
+{
+    int i,j,somma;
+    int matrice [3][3];
+    somma=0;
+    for (i=0;i<3;i++)
+    for(j=0;j<3;j++)
+    {
+        cout << "Inserisci l'elemento " << i+1 << "," << j+1 << " della matrice: ";
+        cin >> matrice[i][j];
+        somma=somma+matrice[i][j];
+    }
+    cout << "La matrice e':" << endl;
+    for (i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            cout << matrice [i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << "La somma e' : "<< somma << endl
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
